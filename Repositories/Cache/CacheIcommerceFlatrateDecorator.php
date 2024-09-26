9aa64b0b911689e07dc92a282b971566ac276709
William <williamverde10@gmail.com>
2024-09-26 12:38:28 -0400

    Delete: remember in method calculate | Case: Cache

--- Repositories/Cache/CacheIcommerceFlatrateDecorator.php
+++ Repositories/Cache/CacheIcommerceFlatrateDecorator.php
@@ -21,8 +21,8 @@ class CacheIcommerceFlatrateDecorator extends BaseCacheDecorator implements Icom
      */
     public function calculate($parameters, $conf)
     {
-        return $this->remember(function () use ($parameters, $conf) {
-            return $this->repository->calculate($parameters, $conf);
-        });
+        
+        return $this->repository->calculate($parameters, $conf);
+        
     }
 }
